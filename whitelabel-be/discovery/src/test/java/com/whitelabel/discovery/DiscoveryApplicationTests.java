8b687b07e0118a556bf7dc38901c68bb1a5e02a0
Geraldo Luiz <geraldobl58@gmail.com>
2026-09-11 16:28:43 -0300

    Add discovery module and Maven configuration

---
+++ whitelabel-be/discovery/src/test/java/com/whitelabel/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitelabel.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
